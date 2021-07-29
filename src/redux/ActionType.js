ee424d00b6f9172748e2244d6b7d924760d99924
Rahul Gupta <rahul8844@users.noreply.github.com>
2021-07-30 03:56:45 +0530

    initial commit

--- src/redux/ActionType.js
+++
@@ -1,3 +0,0 @@
-export const SET_APPS = 'SET_APPS';
-export const SET_TABLEDATA = 'SET_TABLEDATA';
-export const SET_COLUMNS = 'SET_COLUMNS';
--- src/redux/Store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from 'redux';
-import { RootReducers } from './Reducers';
-
-export const store = createStore(RootReducers);
